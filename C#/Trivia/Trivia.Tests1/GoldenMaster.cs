427b2136aa970d32cdbee6594bb00fd13d9252a1
PETIT OLIVIER p1613946 <p1613946@univ-lyon1.fr>
2017-03-16 17:30:14 +0100

    Golden Master with ApprovalTests

---
+++ C#/Trivia/Trivia.Tests1/GoldenMaster.cs
@@ -0,0 +1,44 @@
+﻿using System; 
+
+using System.IO; 
+using ApprovalTests; 
+
+using ApprovalTests.Reporters; 
+
+using NUnit.Framework; 
+
+​ 
+namespace Trivia.Tests 
+
+{
+
+    [TestFixture] 
+    [UseReporter(typeof(DiffReporter))] 
+
+    public class GoldenMaster 
+
+    { 
+      [Test] 
+
+       public void ShouldNotChange() 
+       { 
+
+           var stringWriter = new StringWriter(); 
+
+           var previousConsoleOut = Console.Out; 
+
+           //redirige la sortie console 
+
+            Console.SetOut(stringWriter); 
+
+            GameRunner.Main(null);
+
+            Console.SetOut(previousConsoleOut); 
+
+            Approvals.Verify(stringWriter.ToString()); 
+
+        } 
+
+    } 
+
+} 
